607c36bbbf0ba523dbd0854455fdfdbc40a63c99
vishal agrawal <va7863502@gmail.com>
2026-01-03 15:41:39 +0530

    End of Section 3

--- API/Program.cs
+++ API/Program.cs
@@ -4,16 +4,18 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(x=>x.AllowAnyHeader().AllowAnyMethod()
+   .WithOrigins("http://localhost:4200","http://localhost:4200"));
 app.MapControllers();
 
 app.Run();
