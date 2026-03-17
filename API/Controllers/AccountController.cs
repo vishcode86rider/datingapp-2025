b4e2b319280ec8067f7b8af09bc4efcab3926cbf
vishal agrawal <va7863502@gmail.com>
2026-03-17 19:00:40 +0530

    End of Section 12

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -19,13 +19,22 @@ public class AccountController(AppDbContext context, ITokenService tokenService)
         if(await EmailExists(registerDto.Email)) return BadRequest("Email taken");
         using var hmac = new HMACSHA512();
 
-        var user=new AppUser
+        var user = new AppUser
         {
             DisplayName=registerDto.DisplayName,
             Email=registerDto.Email,
             PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
-            PasswordSalt = hmac.Key            
+            PasswordSalt = hmac.Key,
+            Member = new Member
+            {
+                DisplayName = registerDto.DisplayName,
+                Gender = registerDto.Gender,
+                City = registerDto.City,
+                Country = registerDto.Country,
+                DateOfBirth = registerDto.DateOfBirth
+            }            
         };
+
         context.Users.Add(user);
         await context.SaveChangesAsync();
 
